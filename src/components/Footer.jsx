bf2feb20fe5762000d2ff6ded19d29170d561293
Chloe <chloezhao86@gmail.com>
2024-06-26 18:14:43 +1000

    fix: resolve conflicts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,21 @@
-import React, {useEffect,useState} from 'react';
-import { FcLike } from "react-icons/fc";
-import { fetchTeams } from '../utils/api';
-
-
+import React, {useEffect, useState} from 'react';
+import {FcLike} from "react-icons/fc";
 
 function Footer() {
-     const [teams,setTeams] = useState([]); 
-
-    useEffect(()=>{
-        try{
-        fetchTeams().then(response =>{
-            
-        const students = response.data.info.students;
-        setTeams([students[9]]);
-        setTeams(teams=>[...teams,students[11]]);  // push students into array.
-        setTeams(teams=>[...teams,students[13]]);
-        
-    });
-    } catch(error){
-    console.log(error);
-    }
-
-},[]); // call api during initial render.
-   
-    
-  return (
-    <div className='footer'>
-    <div>Created with <FcLike /> by </div>
-    {teams.length && teams.map((team, index)=>(
-            <div key={index} className='team'> {team}</div>
-        ))
-    }  
-    </div>
-    
-  );
-    
+    const teamMembers = [
+        'Winnie Thomas',
+        'Aime Shimirwa',
+        'Chloe Zhao',
+    ]
+    return (
+        <div className='footer'>
+            <div>Created with <FcLike/> by</div>
+            {teamMembers.map((team, index) => (
+                <div key={index} className='team'> {team}</div>
+            ))
+            }
+        </div>
+    );
 }
 
 export default Footer;
